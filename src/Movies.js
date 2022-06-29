12debc75a8351e0692b62ae51c1e672937c43ee2
Praveen Kumar <praveenkumarthipparam@gmail.com>
2022-06-28 23:46:33 -0500

    Initial Fetch

--- src/Movies.js
+++ src/Movies.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import { useGlobalContext } from './context'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { useGlobalContext } from "./context";
+import { Link } from "react-router-dom";
 const url =
-  'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'
+  "https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png";
 
 const Movies = () => {
-  return <h2>movies component</h2>
-}
+  return <h2>movies component</h2>;
+};
 
-export default Movies
+export default Movies;
--- src/context.js
+++ src/context.js
@@ -2,12 +2,43 @@ import React, { useState, useContext, useEffect } from "react";
 // make sure to use https
 export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
 
-console.log(API_ENDPOINT);
-
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState({ show: false, msg: "" });
+  const [query, setQuery] = useState("batma");
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async (url) => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+
+      if (data.Response === "True") {
+        setMovies(data);
+        setError({ show: false, msg: "" });
+      } else {
+        setError({ show: true, msg: data.Error });
+      }
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies(`${API_ENDPOINT}&s=${query}`);
+  }, [query]);
+
+  return (
+    <AppContext.Provider value={{ isLoading, error, query, movies, setQuery }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 // make sure use
 export const useGlobalContext = () => {
